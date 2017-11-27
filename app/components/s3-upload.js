9d7f5c781435b3e8497a48ea174223e7b91396b6
shaunwillis-SpectrumReach <shaun.willis@charter.com>
2017-11-27 12:02:35 -0600

    First initial launch

---
+++ app/components/s3-upload.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+import EmberUploader from 'ember-uploader';
+
+export default EmberUploader.FileField.extend({
+
+  url: "http://localhost:3333/sign/profile/images",
+
+  filesDidChange: function() {
+      let uploadUrl = this.get('url'),
+        files     = this.get('files'),
+        uploader  = EmberUploader.S3Uploader.create({
+          url: uploadUrl
+        });
+    if (!Ember.isEmpty(files))
+      uploader.upload(files[0]);
+  }.observes('files')
+});
