ec41ac2ee77eb77298c6f36ed70e5728aae5f366
shaunwillis-SpectrumReach <shaun.willis@charter.com>
2017-12-04 16:00:33 -0600

    More changes to User account Setup

--- app/services/current-user.js
+++ app/services/current-user.js
@@ -1,4 +1,16 @@
 import Service from '@ember/service';
 
 export default Service.extend({
+  store: Ember.inject.service(),
+  session: Ember.inject.service(),
+  pids: null,
+
+  loadCurrentUser() {
+    if (this.get('session.isAuthenticated')) {
+      return this.get('store').queryRecord('pids', { current: true })
+        .then((user) => {
+          this.set('pids', user);
+        });
+    }
+  }
 });
