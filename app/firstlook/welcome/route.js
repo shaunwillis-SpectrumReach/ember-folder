6d290a6ce7626a131de3ac9601c23aba41dc5204
gtarney <gerald.tarney@charter.com>
2017-12-13 18:05:03 -0500

    remove burger and layout welcome route

---
+++ app/firstlook/welcome/route.js
@@ -0,0 +1,10 @@
+import Route from '@ember/routing/route';
+
+import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  // model() {
+  //    return this.store.findAll('pids');
+  // }
+});
