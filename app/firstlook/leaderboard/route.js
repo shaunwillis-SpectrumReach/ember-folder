f7687a45fc959551f33c1ceafd77e01a783ef9ec
gtarney <gerald.tarney@charter.com>
2017-12-19 09:48:08 -0500

    phase 3 work WIP-2

--- app/firstlook/leaderboard/route.js
+++ app/firstlook/leaderboard/route.js
@@ -1,4 +1,22 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+  actions: {
+
+    scrollToTop() {
+      // document.getElementById('message').scrollIntoView();
+      $('html, body').animate({ scrollTop: $('#wrapper').offset().top }, 'slow');
+           return false;
+    },
+
+    resetList() {
+      // document.getElementById('message').scrollIntoView();
+      $('html, body').animate({ scrollTop: $('#wrapper').offset().top }, 'slow');
+           return false;
+    },
+
+  },
+
+
 });
