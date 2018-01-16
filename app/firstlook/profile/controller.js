0bef832c2e4042690e25991c2a7052a4f4d71370
shaunwmusic <shaunwmusic@gmail.com>
2018-01-16 12:50:55 -0600

    working on file upload and forgot i was in master oops

--- app/firstlook/profile/controller.js
+++ app/firstlook/profile/controller.js
@@ -1,62 +1,45 @@
 import Ember from 'ember';
+// import config from './config/environment';
 
 export default Ember.Controller.extend({
   wizard: Ember.inject.service('wizard'),
+  // filesystem: Ember.inject.service(),
   currentUser: Ember.inject.service(),
   session: Ember.inject.service(),
 
-  actions: {
-      saveForm(changesFromForm) {
-        // I changed ...this.get() to an array, because {} threw syntax error
-          this.set('wizard.userInfo', {...this.get('wizard.userInfo'),
-          changesFromForm
-        });
-        // If you want to do stuff with :point_up:
-        let allValues = this.get('wizard.userInfo');
+// saveUser(uploadFile) {
+//   if (uploadFile) {
+//     const user = this.get('currentUser.user');
+//     const token = this.get('session.session.content.authenticated.token');
+//
+//     return this.get('filesystem').fetch(`${config.DS.host}/api/pids/${user.id}`, {
+//       method: 'PATCH',
+//       headers: {
+//         accept: 'application/json',
+//         Authorization: `Bearer ${token}`,
+//       },
+//       body: { ...user.toJSON(), uploadFile },
+//     }).then((res) => res.json())
+//     .then((data) => {
+//       this.store.pushPayload(data);
+//       alert('Image Updated')
+//     });
+//   }
+// },
 
-        let profile = this.store.createRecord('pid', allValues);
-        profile.save().then(() => {
-          profile.save();
-        }).then(() => {
-          this.transitionToRoute('firstlook.register-thanks');
-          // const secretStuff = {
-          //   identification: wizard.email,
-          //   password: wizard.password,
-          // };
-          // console.log(secretStuff);
-          // const authenticator = 'authenticator:jwt';
-          //
-          // this.get('session').authenticate(authenticator, secretStuff)
-          //   .then(() => {
-          //     this.transitionToRoute('firstlook.register-thanks');
-          //   });
-        })
-        .catch(() => {
-          this.transitionToRoute('firstlook.register-thanks');
-
-        });
-
-        // let pids = this.store.createRecord('pids', allValues);
-        // //
-        // pids.save().then(() => {
-        //   return pids.save();
-        // }).then(() => {
-        //   const secretStuff = {
-        //     identification: wizard.email,
-        //     password: wizard.password,
-        //   };
-        //   console.log(secretStuff);
-        //   const authenticator = 'authenticator:jwt';
-        //
-        //   this.get('session').authenticate(authenticator, secretStuff)
-        //     .then(() => {
-        //       this.transitionToRoute('firstlook.register-thanks');
-        //     });
-        // })
-        // .catch(() => {
-        //   console.log('Error Creating User');
-        // });
-
-        }
-        }
+// 
+// actions: {
+//   update(closeForm, formValues) {
+//     this.model.setProperties(formValues);
+//     this.model.save().then(() => {
+//       closeForm();
+//     });
+//   },
+//
+//   choosePic(formValues) {
+//     this.get('filesystem').prompt().then((upload) => {
+//       this.saveUser(upload[0]);
+//     });
+//   }
+// }
   });
