4f3daacd2ca30704b48ae8493470ec864cab4896
shaunwmusic <shaunwmusic@gmail.com>
2018-01-25 09:16:38 -0600

    Must be logged in now to see data

--- app/firstlook/leaderboard/elite-ae400k/route.js
+++ app/firstlook/leaderboard/elite-ae400k/route.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
 
   // beforeModel() {
   //   $('#burger').removeClass('hide');
