ca6810fb2a0c3a7c059f3c24467ddd080cc5e41c
shaunwmusic <shaunwmusic@gmail.com>
2018-02-01 15:49:26 -0600

    Add profile images to standings

--- app/models/ae450k.js
+++ app/models/ae450k.js
@@ -7,5 +7,12 @@ export default DS.Model.extend({
   region: DS.attr('string'),
   managername: DS.attr('string'),
   rank: DS.attr('string'),
-  pids: DS.belongsTo('pids')
+  pids: DS.belongsTo('pids'),
+  profilePicUrl: DS.attr('string'),
+  fullProfilePicUrl: Ember.computed('profilePicUrl', function() {
+    return `https://s3.amazonaws.com/sr-firstlook/public/${this.get('profilePicUrl')}`
+    // return `https://firstlook-staging-api.herokuapp.com/uploads/${this.get('profilePicUrl')}`
+    // return `http://localhost:3333/uploads/${this.get('profilePicUrl')}`
+    // return `https://res.cloudinary.com/http-www-firstlookspectrum-com/image/upload/w_150,h_150,c_thumb,g_face,r_max/v1516647698/${this.get('profilePicUrl')}`
+  }),
 });
--- app/models/eliteae15m.js
+++ app/models/eliteae15m.js
@@ -8,5 +8,12 @@ export default DS.Model.extend({
   managername: DS.attr('string'),
   pids: DS.belongsTo('pids'),
   rank: DS.attr('string'),
-  top5: DS.attr('string')
+  top5: DS.attr('string'),
+  profilePicUrl: DS.attr('string'),
+  fullProfilePicUrl: Ember.computed('profilePicUrl', function() {
+    return `https://s3.amazonaws.com/sr-firstlook/public/${this.get('profilePicUrl')}`
+    // return `https://firstlook-staging-api.herokuapp.com/uploads/${this.get('profilePicUrl')}`
+    // return `http://localhost:3333/uploads/${this.get('profilePicUrl')}`
+    // return `https://res.cloudinary.com/http-www-firstlookspectrum-com/image/upload/w_150,h_150,c_thumb,g_face,r_max/v1516647698/${this.get('profilePicUrl')}`
+  }),
 });
